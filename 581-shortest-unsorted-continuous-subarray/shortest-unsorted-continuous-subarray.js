79eace1e379ba48bf0dbf1bd5107f6002a7c19d1
Aryan <106865458+Aryan1396@users.noreply.github.com>
2026-07-28 14:06:36 +0530

    Time: 22 ms (13.74%) | Memory: 58.9 MB (26.72%) - LeetSync

---
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function(nums) {
+    const sorted = [...nums].sort((a,b) => a- b);
+    let start = nums.length, end = 0;
+
+    for(let i =0; i<nums.length; i++){
+        if(nums[i] !== sorted[i]){
+            start = Math.min(start, i);
+            end = Math.max(end, i);
+        }
+    }
+    return end-start >= 0 ? end - start + 1 : 0;
+};
