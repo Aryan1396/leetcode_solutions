0d7187cc55fea0ef1d522fd185b43f590c2ee9f3
Aryan <106865458+Aryan1396@users.noreply.github.com>
2026-08-01 21:06:10 +0530

    Time: 1070 ms (5.13%) | Memory: 66.6 MB (92.31%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.js
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.js
@@ -2,10 +2,10 @@ let minIncrementForUnique = function(nums){
     nums.sort((a,b) => a-b);
     let moves = 0;
     for(let i=1; i<nums.length; i++){
-        if(nums[i] <= nums[i-1]){
-            moves += nums[i-1] - nums[i] + 1;
-            nums[i] = nums[i-1] + 1
-        }
+       while(nums[i] <= nums[i-1]){
+        nums[i]++;
+        moves++
+       }
 
     }
     return moves
