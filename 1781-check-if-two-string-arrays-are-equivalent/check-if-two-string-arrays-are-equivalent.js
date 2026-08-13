de7a5ffb3448fe46fe6bb0b735ac286b61d0060f
Aryan <106865458+Aryan1396@users.noreply.github.com>
2026-08-13 21:15:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (71.19%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.js
@@ -4,14 +4,6 @@
  * @return {boolean}
  */
 var arrayStringsAreEqual = function(word1, word2) {
-    let arr1 = word1.join("");
-    let arr2 = word2.join("");
-    console.log(arr1, arr2)
-    if(arr1.length !== arr2.length) return false;
-
-    for(let i = 0; i<arr1.length; i++){
-        if(arr1[i] !== arr2[i]) return false
-    }
-    return true
+    return word1.join("") === word2.join("")
     
 };
